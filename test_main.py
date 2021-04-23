2c8d5d936edbcc9398f1f94f9c91afc294f96fec
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2021-04-23 17:17:14 +0200

    improved tests

--- test_main.py
+++ test_main.py
@@ -4,10 +4,16 @@ from main import app
 
 client = TestClient(app)
 
-def test_root():
+def test_root_no_args():
     response = client.get('/')
     assert response.status_code == 400
+
+def test_root_invalid_args():
     response = client.get('/?username=BurnedOlieTree')
     assert response.status_code == 404
+    assert 'Please enter a valid GitHub username' in response.text
+
+def test_root_correct_args():
     response = client.get('/?username=BurnedOliveTree')
     assert response.status_code == 200
+    assert '<li>' in response.text
